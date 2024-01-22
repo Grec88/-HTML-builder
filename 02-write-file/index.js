fba23d1702faaf9c0c410c37e3213b17a3ba73be
grec88 <Lihtinmann88@yandex.ru>
2024-01-22 18:45:33 +0300

    init: 02-read-file task done

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const readline = require('readline');
+const inputStream = fs.createWriteStream('02-write-file/text.txt');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+console.log(
+  'Please, enter some text.\nPress enter to save the input info to the file\n',
+);
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    rl.close();
+    inputStream.end();
+  } else {
+    inputStream.write(input + '\n');
+    console.log(
+      'The text has been saved to the file.\nInput some more text or close the process typing exit or pressing ctrl + c.',
+    );
+  }
+});
+process.on('SIGINT', () => {
+  rl.close();
+});
